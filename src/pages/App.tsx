b016dd1fae32ad959af7d8cd1f97863dfb3c9fac
ThePeoplesTokn <srivera145@gmail.com>
2022-07-29 14:30:01 +0000

    added my project

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -37,21 +37,21 @@ const BodyWrapper = styled.div`
   overflow-x: hidden;
   z-index: 1;
   justify-content: center;
-  // background-image: url('/images/group-pancake.svg');
+  // background-image: url('/images/charityaddress.png');
   // background-repeat: no-repeat;
-  // background-position: bottom 24px center;
+  // background-position: bottom -400px center;
   // background-size: 90%;
 
   ${({ theme }) => theme.mediaQueries.xs} {
-    background-size: auto;
+    // background-size: auto;
   }
 
   ${({ theme }) => theme.mediaQueries.lg} {
-    // background-image: url('/images/arch-${({ theme }) => (theme.isDark ? 'dark' : 'light')}.svg'),
-    //   url('/images/left-pancake.svg'), url('/images/right-pancake.svg');
-    // background-repeat: no-repeat;
-    // background-position: center 420px, 10% 230px, 90% 230px;
-    // background-size: contain, 266px, 266px;
+    background-image: url('/images/${({ theme }) => (theme.isDark ? 'dark' : 'light')}'),
+       url('/images/swap-left.png'), url('/images/swap-right.png');
+     background-repeat: no-repeat;
+     background-position: center 420px, 10% 230px, 90% 230px;
+     background-size: contain, 266px, 266px;
     min-height: 90vh;
   }
 `
